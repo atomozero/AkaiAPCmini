55b5cda47916ce2983b8f6b2c0197d1c1c2b131b
atomozero <bernardi.andrea@gmail.com>
2025-10-04 16:47:53 +0200

    Clean up build artifacts and temporary files
    - Remove auto-generated dependency files (*.d)
    - Remove development validation script

--- build/obj/apc_mini_test.d
+++
@@ -1,2 +0,0 @@
-obj/apc_mini_test.o obj/apc_mini_test.d: ../src/apc_mini_test.cpp \
- ../src/usb_raw_midi.h ../src/apc_mini_defs.h
--- build/obj/usb_haiku_midi.d
+++
@@ -1,2 +0,0 @@
-obj/usb_haiku_midi.o obj/usb_haiku_midi.d: ../src/usb_haiku_midi.cpp \
- ../src/usb_raw_midi.h ../src/apc_mini_defs.h
